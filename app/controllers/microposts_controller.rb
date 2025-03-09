27496a225cc5406ea70762078f6accad01fde6f0
zhwaksij <147130190+Wak8810@users.noreply.github.com>
2025-03-09 06:37:40 +0000

    基本機能の作成完了/見た目もほとんど完了

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ class MicropostsController < ApplicationController
   end
 
   def index 
-    @microposts = Micropost.all
+    @microposts = Micropost.order(id: :desc) #降順で並び替える
   end
 
   def new
@@ -17,21 +17,34 @@ class MicropostsController < ApplicationController
     if @micropost.save
       redirect_to microposts_path
     else
-      flash[:danger] = "No Content"
+      if @micropost.content.length <= 0
+        flash[:danger] = "内容がありません"
+      elsif @micropost.content.length > 140
+        flash[:danger] = "140文字を超えています"
+      end
       render 'new', status: :unprocessable_entity
     end
   end
 
   def edit
-    
+    @micropost = Micropost.find(params[:id])
   end
 
   def update
-    
+    @micropost = Micropost.find(params[:id])
+    if @micropost.update(micropost_params)
+      flash[:success] = "Post Updated"
+      redirect_to microposts_path
+    else
+      flash[:danger] = "140文字を超えています！"
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
-    
+    @micropost = Micropost.find(params[:id]).destroy
+    flash[:success] = "Post deleted"
+    redirect_to root_url, status: :see_other
   end
 
   private
